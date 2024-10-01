59f69b570ba89936b425fa60b7458497b044924f
poteatc <poteatc@outlook.com>
2024-10-01 16:27:01 -0400

    Encapsulated code to make it more readable

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -20,56 +20,11 @@ public class Main {
                 continue;
             }
             if (input == 1) {
-                // Get the necessary inputs from user for mortgage calculations
-                System.out.print("\nEnter the principal amount: $");
-                int principal = scanner.nextInt();
-                System.out.print("\nEnter the interest rate as a percentage: %");
-                double interestRate = scanner.nextDouble();
-                System.out.print("\nEnter the loan length in years(Integer): ");
-                int loanLength = scanner.nextInt();
-
-                // Instantiate the mortgageCalculator object with the given user input
-                MortgageCalculator mortgageCalculator = new MortgageCalculator(principal, interestRate, loanLength);
-
-                // Print out user inputs and final calculations
-                System.out.printf("A $%d loan at %.3f%% interest for %d years would have a $%.2f/mo payment " +
-                        "with a total interest of $%.2f",
-                        principal, interestRate, loanLength, mortgageCalculator.getMonthlyPayment(), mortgageCalculator.getTotalInterest());
-                System.out.println("\n\n");
+                useMortgageCalculator(scanner);
             } else if (input == 2) {
-                // Get the necessary inputs from user for a CD (certificate of deposit)
-                System.out.print("\nEnter the deposit amount: $");
-                int deposit = scanner.nextInt();
-                System.out.print("\nEnter the annual interest rate as a percentage: %");
-                double annualInterestRate = scanner.nextDouble();
-                System.out.print("\nEnter the loan length in years: ");
-                int loanLength = scanner.nextInt();
-
-                // Instantiate the CDValueCalculator object with the given user input
-                CDValueCalculator cdValueCalculator = new CDValueCalculator(deposit, annualInterestRate, loanLength);
-
-                // Print out user inputs and final calculations
-                System.out.printf("If you deposit $%d in a CD that earns %.2f%% interest and matures in " +
-                        "%d years, your CD's ending balance will be $%.2f and you would have earned " +
-                        "$%.2f in interest", deposit, annualInterestRate, loanLength, cdValueCalculator.getFutureValue(), cdValueCalculator.getInterestAccrued());
-                System.out.println("\n\n");
+                useCDValueCalculator(scanner);
             } else if (input == 3) {
-                // Get the necessary inputs from user for an annuity
-                System.out.println("Enter the monthly payout: ");
-                int monthlyPayout = scanner.nextInt();
-                System.out.println("Enter the expected interest rate: %");
-                double interestRate = scanner.nextDouble();
-                System.out.println("Enter the amount of years: ");
-                int years = scanner.nextInt();
-
-                // Instantiate an AnnuityCalculator object with the given user input
-                AnnuityCalculator annuityCalculator = new AnnuityCalculator(monthlyPayout, interestRate, years);
-
-                // Print out user inputs and final calculations
-                System.out.printf("To fund an annuity that pays $%d monthly for %d years and earns" +
-                        " an expected %.2f%% interest, you would need to invest $%.2f today."
-                        , monthlyPayout, years, interestRate, annuityCalculator.getPresentValue());
-                System.out.println("\n\n");
+                useAnnuityCalculator(scanner);
             } else if (input == 4) {
                 // Exit if user enters the integer 4
                 break;
@@ -87,6 +42,63 @@ public class Main {
             }
         } while (input != 4);
         System.out.println("Exiting calculator...");
+    }
+
+    public static void useAnnuityCalculator(Scanner scanner) {
+        // Get the necessary inputs from user for an annuity
+        System.out.println("Enter the monthly payout: ");
+        int monthlyPayout = scanner.nextInt();
+        System.out.println("Enter the expected interest rate: %");
+        double interestRate = scanner.nextDouble();
+        System.out.println("Enter the amount of years: ");
+        int years = scanner.nextInt();
+
+        // Instantiate an AnnuityCalculator object with the given user input
+        AnnuityCalculator annuityCalculator = new AnnuityCalculator(monthlyPayout, interestRate, years);
+
+        // Print out user inputs and final calculations
+        System.out.printf("To fund an annuity that pays $%d monthly for %d years and earns" +
+                        " an expected %.2f%% interest, you would need to invest $%.2f today."
+                , monthlyPayout, years, interestRate, annuityCalculator.getPresentValue());
+        System.out.println("\n\n");
+    }
+
+    private static void useCDValueCalculator(Scanner scanner) {
+        // Get the necessary inputs from user for a CD (certificate of deposit)
+        System.out.print("\nEnter the deposit amount: $");
+        int deposit = scanner.nextInt();
+        System.out.print("\nEnter the annual interest rate as a percentage: %");
+        double annualInterestRate = scanner.nextDouble();
+        System.out.print("\nEnter the loan length in years: ");
+        int loanLength = scanner.nextInt();
+
+        // Instantiate the CDValueCalculator object with the given user input
+        CDValueCalculator cdValueCalculator = new CDValueCalculator(deposit, annualInterestRate, loanLength);
+
+        // Print out user inputs and final calculations
+        System.out.printf("If you deposit $%d in a CD that earns %.2f%% interest and matures in " +
+                "%d years, your CD's ending balance will be $%.2f and you would have earned " +
+                "$%.2f in interest", deposit, annualInterestRate, loanLength, cdValueCalculator.getFutureValue(), cdValueCalculator.getInterestAccrued());
+        System.out.println("\n\n");
+    }
+
+    private static void useMortgageCalculator(Scanner scanner) {
+        // Get the necessary inputs from user for mortgage calculations
+        System.out.print("\nEnter the principal amount: $");
+        int principal = scanner.nextInt();
+        System.out.print("\nEnter the interest rate as a percentage: %");
+        double interestRate = scanner.nextDouble();
+        System.out.print("\nEnter the loan length in years(Integer): ");
+        int loanLength = scanner.nextInt();
+
+        // Instantiate the mortgageCalculator object with the given user input
+        MortgageCalculator mortgageCalculator = new MortgageCalculator(principal, interestRate, loanLength);
+
+        // Print out user inputs and final calculations
+        System.out.printf("A $%d loan at %.3f%% interest for %d years would have a $%.2f/mo payment " +
+                        "with a total interest of $%.2f",
+                principal, interestRate, loanLength, mortgageCalculator.getMonthlyPayment(), mortgageCalculator.getTotalInterest());
+        System.out.println("\n\n");
     }
 
     // Calculator main menu
